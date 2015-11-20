2851201a40bb2b9493d71e5cf5a6227155836880
benjisora <benji.saugues@gmail.com>
2015-11-20 12:30:15 +0100

    Updating Zend App to implement authentication

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -16,6 +16,7 @@ class Module
 {
     public function onBootstrap(MvcEvent $e)
     {
+		$app = $e->getApplication();
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
